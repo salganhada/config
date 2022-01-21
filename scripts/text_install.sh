08830b180cf7cefb1f87a492b7719bea01494a69
salganhada <jfgazpar@gmail.com>
2022-01-21 14:32:38 +0000

    first installation scripts

---
+++ scripts/text_install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# adding non-free repositories to the sources.list
+sudo sed -i 's/main/main contrib non-free/gI' /etc/apt/sources.list
+
+
+sudo apt install man -yy
+sudo apt install nvim -yy
+sudo apt install tmux -yy
+sudo apt install build-essential -yy
