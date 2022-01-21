f94624bb4b93459b62534076fb433eb91ebb0ea3
joao gaspar <jgazpar@debian.dwm>
2022-01-21 09:59:14 -0800

    nvim to neovim

--- scripts/text_install.sh
+++ scripts/text_install.sh
@@ -5,6 +5,6 @@ sudo sed -i 's/main/main contrib non-free/gI' /etc/apt/sources.list
 
 
 sudo apt install man -yy
-sudo apt install nvim -yy
+sudo apt install neovim -yy
 sudo apt install tmux -yy
 sudo apt install build-essential -yy
